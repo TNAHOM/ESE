ddfcc3c570709712ceb0031171773f916bfeede9
TNAHOM <nahomt2419@gmail.com>
2023-04-05 15:36:13 +0100

    Fixed renaming directory and gitignore

--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, redirect, url_for, flash, request, Blueprint
-from market.models import User
-from market.forms import RegisterForm, LoginForm
+from main.models import User
+from main.forms import RegisterForm, LoginForm
 from flask_login import login_user, logout_user, login_required, current_user
 
 views = Blueprint('views', __name__)
